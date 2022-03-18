b36aedc70bf0014ff371b6e7548c4cbc74c1a02d
mohammed amine jebbour <jebbour60@gmail.com>
2022-03-18 16:17:36 +0100

    TP8"modifié"

---
+++ src/main/java/cigma/pfe/services/FactureServiceImpl.java
@@ -0,0 +1,25 @@
+package cigma.pfe.services;
+import cigma.pfe.models.Facture;
+import cigma.pfe.repositories.FactureRepository;
+import java.util.List;
+public class FactureServiceImpl implements FactureService {
+
+    FactureRepository factureRepository;
+
+
+    public Facture create(Facture f) {
+        return factureRepository.create(f);
+    }
+
+    public List<Facture> read() {
+        return factureRepository.read();
+    }
+
+    public void delete(long id) {
+        factureRepository.deleteFactureById(id);
+    }
+
+    public Facture update(Facture f) {
+        return factureRepository.update(f);
+    }
+}
--- src/main/java/ma/education/tp6/annotations/Calculatrice.java
+++
@@ -1,4 +0,0 @@
-package ma.education.tp6.annotations;
-@Programmer(id=10, name="salam")
-public class Calculatrice {
-}
